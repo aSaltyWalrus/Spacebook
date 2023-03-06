3181df29e01a1db0cd13411af021bd2bcc895dcb
Alexander Murray <48931401+aSaltyWalrus@users.noreply.github.com>
2023-03-06 14:12:34 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import 'react-native-gesture-handler';
+
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Friends from './pages/Friends';
+import UserEdit from './pages/UserEdit';
+import CameraPage from './pages/CameraPage';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// 3 different classes for embedment of Navigator
+
+class App extends Component{
+	render(){
+		return (
+			<Tab.Navigator  screenOptions={{headerShown: false}}>
+				<Tab.Screen name="Profile" component={Profile} />
+				<Tab.Screen name="Edit Profile" component={UserEdit} />
+				<Tab.Screen name="Camera" component={CameraPage} />
+				<Tab.Screen name="User Explorer" component={BrowseUsers} />
+			</Tab.Navigator>
+		);
+	}
+}
+
+class BrowseUsers extends Component{
+	render(){
+		return (
+			<Stack.Navigator>
+				<Stack.Screen name="Friends" component={Friends} />
+				<Stack.Screen name="User" component={Profile} />
+			</Stack.Navigator>
+		);
+	}
+} 
+
+class UserAccess extends Component{
+	render(){
+		return (
+			<NavigationContainer>
+				<Stack.Navigator screenOptions={{headerShown: false}}>
+					<Stack.Screen name="Login" component={Login} />
+					<Stack.Screen name="SignUp" component={SignUp} />
+					<Stack.Screen name="App" component={App} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		);
+	}
+} 
+
+export default UserAccess;
